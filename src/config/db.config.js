02abce384475e7e8eeea89e4ec19b2886843988d
AdeoluX <73498971+AdeoluX@users.noreply.github.com>
2025-05-15 22:30:20 +0100

    fix

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const dbConnect = mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => console.log('Connected to MongoDB'));
+const dbConnect = mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  });
 
 module.exports = dbConnect;
